8b212cd824a0a9823ad4c683e771d7d53664c678
jfilipe-dev <jfilipe1511@gmail.com>
2020-04-29 21:40:53 -0300

    Added transactions listing

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,9 +10,37 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  // public async getBalance(): Promise<Balance> {
-  //   // TODO
-  // }
+  public async getBalance(): Promise<Balance> {
+    const transactions = await this.find();
+
+    const balance = transactions.reduce(
+      (accumulator, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += Number(transaction.value);
+            break;
+
+          case 'outcome':
+            accumulator.outcome += Number(transaction.value);
+            break;
+
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
+  }
 }
 
 export default TransactionsRepository;
--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import { getCustomRepository } from 'typeorm';
+
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
@@ -8,7 +10,12 @@ import CreateTransactionService from '../services/CreateTransactionService';
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
@@ -22,8 +29,6 @@ transactionsRouter.post('/', async (request, response) => {
     type,
     category,
   });
-
-  console.log(transaction);
 
   return response.json(transaction);
 });
